bf2a85b7b145f7e35ffe48bad259a6ab9dd34b0b
GusBTB <gustavocs81@gmail.com>
2023-02-24 12:42:35 -0300

    :feat: created delete announcement modal :hammer: fixed user provider, autologin

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,77 +1,92 @@
-import Footer from "../../components/Footer"
-import { ContainerAuction, ContainerBannerHome, ContainerListCar, ContainerListMotorcycle } from "./styles"
-import { useContext, useEffect, useRef, useState } from "react"
-import { AnnouncementContext } from "../../context/AnnouncementContext"
-import ProductCard from "../../components/ProductCard"
-import ProductionCardAuction from "../../components/ProductionCardAuction"
-import Header from "../../components/Header"
-import {motion} from "framer-motion"
+import Footer from "../../components/Footer";
+import {
+  ContainerAuction,
+  ContainerBannerHome,
+  ContainerListCar,
+  ContainerListMotorcycle,
+} from "./styles";
+import { useContext, useEffect, useRef, useState } from "react";
+import { AnnouncementContext } from "../../context/AnnouncementContext";
+import ProductCard from "../../components/ProductCard";
+import ProductionCardAuction from "../../components/ProductionCardAuction";
+import Header from "../../components/Header";
+import { motion } from "framer-motion";
+import { UserContext } from "../../context/userContext";
 
+const Home = () => {
+  const { user } = useContext(UserContext);
+  console.log(user);
 
-const Home = () =>{
+  const { cars, motorcycles, auctions, goProduct } =
+    useContext(AnnouncementContext);
+  const [width, setWidth] = useState(0);
 
-    const {cars,motorcycles, auctions, goProduct} = useContext(AnnouncementContext)
-    const [width, setWidth] = useState(0)
+  const carousel = useRef<HTMLUListElement>(null);
+  useEffect(() => {
+    setWidth(carousel.current!.scrollWidth - carousel.current!.offsetWidth);
+  }, []);
 
-    const carousel = useRef<HTMLUListElement>(null)
-    useEffect(()=>{
-        setWidth(carousel.current!.scrollWidth - carousel.current!.offsetWidth)
-    },[])
-
-    return(
-        <div>
-            <Header />
-            <ContainerBannerHome>
-                <div className="containerBannerContent ">
-                    
-                    <h2> Velocidade e experiência em um lugar feito para você </h2>
-                    <span> Um ambiente feito para você explorar o seu melhor </span>
-                    <div className="containerButton">
-                        <button>Carros</button>
-                        <button>Motos</button>
-                    </div>
-                    
-
-                </div>
-            </ContainerBannerHome>
-            <ContainerAuction>
-                <h4>Leilão</h4>
-                <motion.ul ref={carousel} className="carousel" whileTap={{cursor:"grabbing"}}>
-                    {auctions.map((auction)=>{
-                        return(
-                            <motion.li drag="x" dragConstraints={{right:0, left: -width}} className="inner" key={auction.id} ><ProductionCardAuction product={auction} /></motion.li>
-                        )
-                    })}
-                    
-                    
-                </motion.ul>
-            </ContainerAuction>
-            <ContainerListCar>
-            <h4>Carros</h4>
-            <ul>               
-            {cars.map((car)=>{
-               return (
-                   <li onClick={goProduct} key={car.id}><ProductCard  product={car} /></li>
-               ) 
-            })}
-            </ul>
-                
-            </ContainerListCar>
-            <ContainerListMotorcycle>
-            <h4>Motos</h4>
-            <ul>
-                {motorcycles.map((motorcycle)=>{
-                  return(
-                    <li key={motorcycle.id}><ProductCard  product={motorcycle} /></li>
-                  ) 
-                    
-                })}
-            </ul>
-            </ContainerListMotorcycle>
-            <Footer />
+  return (
+    <div>
+      <Header />
+      <ContainerBannerHome>
+        <div className="containerBannerContent ">
+          <h2> Velocidade e experiência em um lugar feito para você </h2>
+          <span> Um ambiente feito para você explorar o seu melhor </span>
+          <div className="containerButton">
+            <button>Carros</button>
+            <button>Motos</button>
+          </div>
         </div>
-    )
+      </ContainerBannerHome>
+      <ContainerAuction>
+        <h4>Leilão</h4>
+        <motion.ul
+          ref={carousel}
+          className="carousel"
+          whileTap={{ cursor: "grabbing" }}
+        >
+          {auctions.map((auction) => {
+            return (
+              <motion.li
+                drag="x"
+                dragConstraints={{ right: 0, left: -width }}
+                className="inner"
+                key={auction.id}
+              >
+                <ProductionCardAuction product={auction} />
+              </motion.li>
+            );
+          })}
+        </motion.ul>
+      </ContainerAuction>
+      <ContainerListCar>
+        <h4>Carros</h4>
+        <ul>
+          {cars.map((car) => {
+            return (
+              <li onClick={goProduct} key={car.id}>
+                <ProductCard product={car} />
+              </li>
+            );
+          })}
+        </ul>
+      </ContainerListCar>
+      <ContainerListMotorcycle>
+        <h4>Motos</h4>
+        <ul>
+          {motorcycles.map((motorcycle) => {
+            return (
+              <li key={motorcycle.id}>
+                <ProductCard product={motorcycle} />
+              </li>
+            );
+          })}
+        </ul>
+      </ContainerListMotorcycle>
+      <Footer />
+    </div>
+  );
+};
 
-}
-
-export default Home
+export default Home;
