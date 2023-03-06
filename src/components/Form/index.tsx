95bb5beb82643bbcbb4cd73b8f7ce46cbd2048b4
Raphael Gloria <rapha.gloria@gmail.com>
2023-03-06 12:42:17 -0300

    :hammer: register page is mobile friendly now

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,18 +1,32 @@
-import { FormHTMLAttributes, ReactNode } from "react"
-import { Container } from "./style"
-import Text from "../Text"
+import { FormHTMLAttributes, ReactNode } from "react";
+import { Container } from "./style";
+import Text from "../Text";
 export interface IFormProps extends FormHTMLAttributes<HTMLFormElement> {
-    section1?: string
-    children: ReactNode
-    title: string
+  section1?: string;
+  children: ReactNode;
+  title: string;
 }
-const Form = ({section1, children, title,...rest}:IFormProps) => {
-    return(
-        <Container {...rest}>
-            <Text element="h2" family="Lexend" weight={500} size={24} color="black-1" description={title}/>
-            <Text element="h3" family="Inter" weight={500} size={14} color="black-1" description={section1}/>
-            {children}
-        </Container>
-    )
-}
-export default Form
+const Form = ({ section1, children, title, ...rest }: IFormProps) => {
+  return (
+    <Container {...rest}>
+      <Text
+        element="h2"
+        family="Lexend"
+        weight={500}
+        size={24}
+        color="black-1"
+        description={title}
+      />
+      <Text
+        element="h3"
+        family="Inter"
+        weight={500}
+        size={14}
+        color="black-1"
+        description={section1}
+      />
+      {children}
+    </Container>
+  );
+};
+export default Form;
--- src/components/Form/style.ts
+++ src/components/Form/style.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.form`
-    padding: 20px;
-    background-color: var(--grey-10);
-    border-radius: 4px;
-    padding: 44px 48px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 32px;
-    width: 20rem;
-`
+  width: 80%;
+
+  max-width: 412px;
+
+  box-sizing: border-box;
+  background-color: var(--grey-10);
+  border-radius: 4px;
+  padding: 35px 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+`;
